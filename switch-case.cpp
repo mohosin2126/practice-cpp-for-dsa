c89ca1d18519dbe5f9f677a8c569edcde4028ac4
mohosin2126 <mohosin2126@gmail.com>
2024-11-17 19:46:25 +0600

    switch case done

---
+++ switch-case.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 5;
+    switch (x % 2)
+    {
+    case 0:
+        cout << "Even\n";
+        break;
+
+    case 1:
+        cout << "Odd";
+        break;
+    }
+    return 0;
+}

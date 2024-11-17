e8b4b406201c10751881f3ef5d1a7eba265399a7
mohosin2126 <mohosin2126@gmail.com>
2024-11-17 11:48:05 +0600

    min max swap done

---
+++ min-max-swap.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+    cout << max(x, y) << endl;
+    cout << min(x, y) << endl;
+    swap(x, y);
+    cout << x << " " << y;
+    return 0;
+}

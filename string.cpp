f0f11d4a9a8730b6ee6cfaa93f04a2e0198e3476
mohosin2126 <mohosin2126@gmail.com>
2024-12-01 14:23:30 +0600

    string basic done

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cin.ignore();
+    char s[100];
+    cin.getline(s, 100);
+    cout << x << endl<< s;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,11 @@
- // func();
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 25;
+    int y = 8;
+   swap(x,y);
+   cout<<x<<y<<endl;
+
+    return 0;
+}

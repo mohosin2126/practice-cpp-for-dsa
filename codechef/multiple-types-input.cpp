58fc4a97b579b18c84e1ac8615b3c75d9a5a6ac5
mohosin2126 <mohosin2126@gmail.com>
2025-01-01 18:58:26 +0600

    multiple types input has been solved

---
+++ codechef/multiple-types-input.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B;
+        string S;
+        cin >> A >> B;
+        cin >> S;
+        cout << A << " " << B << " " << S << endl;
+    }
+}

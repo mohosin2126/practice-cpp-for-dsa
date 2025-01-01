ce91720b1b1cd5855304ca914352e190074508f4
mohosin2126 <mohosin2126@gmail.com>
2025-01-01 18:52:15 +0600

    data types has been solved

---
+++ codechef/data-types.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a = -5;
+    cout << a << endl;
+
+    float b = 4.0;
+    cout << b << endl;
+
+    string s = "abcd";
+    cout << s << endl;
+
+    int arr[4] = {7, 8, 9, 10};
+    cout << arr[2];
+}

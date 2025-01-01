654ecb9e9ef02571d410bacadaf90389e32a5fb2
mohosin2126 <mohosin2126@gmail.com>
2025-01-01 18:57:17 +0600

    multiple lines has been solved

---
+++ codechef/multiple-lines-input.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int A, B, C, D, E;
+
+        cin >> A >> B;
+
+        cin >> C >> D >> E;
+
+        cout << A << " " << B << " " << C << " " << D << " " << E << endl;
+    }
+}

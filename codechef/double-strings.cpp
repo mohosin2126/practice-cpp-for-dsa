d0e2b879b33b7bcb4dc09be6fd63f9e1f42eb3d6
mohosin2126 <mohosin2126@gmail.com>
2025-01-02 21:24:10 +0600

    double strings has been solved

---
+++ codechef/double-strings.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, X;
+        cin >> S;
+        X = S + S;
+        cout << X << endl;
+    }
+}

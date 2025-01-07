0bc5871d2c2401bba72c35b9f0f4cc39530ac1bd
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-01-07 20:05:54 +0600

    age limit has been solved

---
+++ codechef/age-limit.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int X, Y, A;
+        cin >> X >> Y >> A;
+        if (A >= X && A < Y)
+        {
+         
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}

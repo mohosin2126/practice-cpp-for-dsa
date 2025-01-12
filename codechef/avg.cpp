06019c00713fe733c766a2d291e34c44a9df1642
Md Mohosin Ali Shah <mohosin2126@gmail.com>
2025-01-12 21:50:19 +0600

    avg has been solved

---
+++ codechef/avg.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int A = 15;
+    int C = 17;
+    if (__ == 0 && __ == 0)
+    {
+        cout << "Both A and C are even" << endl;
+    }
+    else if (__ != 0 and __ != 0)
+    {
+        cout << "Both A and C are odd" << endl;
+    }
+    else
+    {
+        cout << "A is odd and C is even or vice versa" << endl;
+    }
+    A = 15;
+    C = 18;
+    if (__ == 0 && __ == 0)
+    {
+        cout << "Both A and C are even" << endl;
+    }
+    else if (__ != 0 && __ != 0)
+    {
+        cout << "Both A and C are odd" << endl;
+    }
+    else
+    {
+        cout << "A is odd and C is even or vice versa" << endl;
+    }
+    return 0;
+}
